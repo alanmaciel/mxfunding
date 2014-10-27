d37c294703e04c1772f239784eb829747df237e6
alan m <alan@ubernetics.com>
2014-10-26 20:41:47 -0600

    Added Crowdfund file

---
+++ crowdfund.rb
@@ -0,0 +1,9 @@
+project1 = "ABC"
+project2 = "LMN"
+project3 = "XYZ"
+
+funding1 = 10000
+
+puts "Project #{project1} has $#{funding1} in funding."
+
+puts "\nProjects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
