f13b5b79f73f8a4ce0533c4e2961bffe6c5c344e
alan m <alan@ubernetics.com>
2014-11-07 15:56:47 -0600

    Added funding amount to each project, shows how much funding each project have so far

--- crowdfund.rb
+++ crowdfund.rb
@@ -3,7 +3,15 @@ project2 = "LMN"
 project3 = "XYZ"
 
 funding1 = 10000
+funding2 = 15000
+funding3 = 1200000
 
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "Welcome to CrowdFunding".upcase.center(80, "*")
+puts "At this time #{formatted_time} the projects have:".center(80)
+puts "".center(80, "*")
 puts "Project #{project1} has $#{funding1} in funding."
+puts "Project #{project2} has $#{funding2} in funding."
+puts "Project #{project3} has $#{funding3} in funding."
 
-puts "\nProjects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
