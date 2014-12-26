9ad5b9f1ce4f1936907d3c7ecce8e461fb2a0042
alan m <alan@ubernetics.com>
2014-12-25 20:12:17 -0600

    OO redesign to crowdfunding program.

--- crowdfund.rb
+++ crowdfund.rb
@@ -27,31 +27,39 @@ class Project
   end
 end
 
+class StartUp
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @projects = []
+  end
+
+  def add_project(project)
+    @projects << project
+  end
+
+  def request_funding
+    @projects.each do |project|
+      puts project
+    end
+
+    @projects.each do |project|
+      project.add_funds
+      project.remove_funds
+      puts project
+    end
+  end
+end
 
 project1 = Project.new("ABC", 100, 10000)
 project2 = Project.new("LMN", 200, 15000)
 project3 = Project.new("XYZ", 300, 1200000)
 project4 = Project.new("APP", 1500, 99900)
 
-projects = [project1, project2, project3]
-
-projects.pop
-projects.push(project4)
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-puts "Welcome to CrowdFunding".upcase.center(80, "*")
-puts "At this time #{formatted_time} the #{projects.size} projects have:".center(80)
-puts "".center(80, "*")
-
-projects.each do |project|
-  puts project
-end
-
-puts "".center(80, "*")
-
-projects.each do |project|
-  project.add_funds
-  project.add_funds
-  project.remove_funds
-  puts project
-end
+vc_friendly = StartUp.new("VC-Friendly Start-up Projects")
+vc_friendly.add_project(project1)
+vc_friendly.add_project(project2)
+vc_friendly.add_project(project3)
+vc_friendly.add_project(project4)
+vc_friendly.request_funding
